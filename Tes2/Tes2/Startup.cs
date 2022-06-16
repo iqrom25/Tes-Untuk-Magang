2fd17b73f5d33752d697f24165961fa55510aa10
muhammad iqrom <miqrom25@gmail.com>
2022-06-16 10:35:04 +0800

    Soal Tes2 done

--- Tes2/Tes2/Startup.cs
+++ Tes2/Tes2/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tes2.Data;
 
 namespace Tes2
 {
@@ -26,12 +27,8 @@ namespace Tes2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Tes2", Version = "v1" });
-            });
+            services.AddSingleton<IRepository, Repository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +37,6 @@ namespace Tes2
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tes2 v1"));
             }
 
             app.UseHttpsRedirection();
